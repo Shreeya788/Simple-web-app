b61aecc9ed44b3c3313acd088bb5ee75d9c73974
Shreeya Shrestha <shresthashreeya788@gmail.com>
2023-07-15 23:48:45 +0545

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import css from "vite-plugin-css"; // Import the vite-plugin-css
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    css(), // Add the vite-plugin-css as a plugin
+  ],
   optimizeDeps: {
     include: ["bootstrap"],
   },
